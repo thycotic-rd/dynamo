80a6a17a704fa6fed0a34deed785b925fb0e7ad4
masres <mal.asres@thycotic.com>
2019-03-17 18:46:51 -0400

    - Add tx with context

--- substitute.go
+++ substitute.go
@@ -11,7 +11,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 
-	"github.com/guregu/dynamo/internal/exprs"
+	"github.com/thycotic-rd/dynamo/internal/exprs"
 )
 
 // subber is a "mixin" for operators for keep track of subtituted keys and values
